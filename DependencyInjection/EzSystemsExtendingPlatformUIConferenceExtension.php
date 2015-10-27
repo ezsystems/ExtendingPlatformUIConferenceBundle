0bd5e98798c742f4c132c3701c061b34ed5a3e2a
Damien Pobel <dp@ez.no>
2015-10-27 17:21:48 +0100

    Added the handling of the PlatformUI configuration

--- DependencyInjection/EzSystemsExtendingPlatformUIConferenceExtension.php
+++ DependencyInjection/EzSystemsExtendingPlatformUIConferenceExtension.php
@@ -7,12 +7,18 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ContextualizerInterface;
+use Symfony\Component\Config\Resource\FileResource;
+
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class EzSystemsExtendingPlatformUIConferenceExtension extends Extension
+class EzSystemsExtendingPlatformUIConferenceExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +30,43 @@ class EzSystemsExtendingPlatformUIConferenceExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        // EzSystemsExtendingPlatformUIConferenceBundle will process a siteaccess aware configuration
+        $processor = new ConfigurationProcessor( $container, 'ez_systems_extending_platform_ui_conference' );
+    }
+
+    public function prepend( ContainerBuilder $container )
+    {
+        // make sure Assetic can handle the assets (mainly the CSS files)
+        $container->prependExtensionConfig( 'assetic', array( 'bundles' => array( 'EzSystemsExtendingPlatformUIConferenceBundle' ) ) );
+
+        // prepend the yui.yml and the css.yml from EzSystemsExtendingPlatformUIConferenceBundle
+        // of course depending on your needs you can remove the handling of yui.yml or css.yml
+        $this->prependYui( $container );
+        $this->prependCss( $container );
+    }
+
+    private function prependYui( ContainerBuilder $container )
+    {
+        $container->setParameter(
+            'extending_platformui.public_dir',
+            'bundles/ezsystemsextendingplatformuiconference'
+        );
+        $yuiConfigFile = __DIR__ . '/../Resources/config/yui.yml';
+        $config = Yaml::parse( file_get_contents( $yuiConfigFile ) );
+        $container->prependExtensionConfig( 'ez_platformui', $config );
+        $container->addResource( new FileResource( $yuiConfigFile ) );
+    }
+
+    private function prependCss( ContainerBuilder $container )
+    {
+        $container->setParameter(
+            'extending_platformui.css_dir',
+            'bundles/ezsystemsextendingplatformuiconference/css'
+        );
+        $cssConfigFile = __DIR__ . '/../Resources/config/css.yml';
+        $config = Yaml::parse( file_get_contents( $cssConfigFile ) );
+        $container->prependExtensionConfig( 'ez_platformui', $config );
+        $container->addResource( new FileResource( $cssConfigFile ) );
     }
 }
