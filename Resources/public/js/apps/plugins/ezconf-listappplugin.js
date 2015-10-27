e9694bff40f62fae1e21267db45409476b51e518
Damien Pobel <dp@ez.no>
2015-10-27 18:33:51 +0100

    Added a route to the list
    For now, it displays the dashboard

--- Resources/public/js/apps/plugins/ezconf-listappplugin.js
+++ Resources/public/js/apps/plugins/ezconf-listappplugin.js
@@ -10,6 +10,17 @@ YUI.add('ezconf-listapplugin', function (Y) {
 
             console.log("Hey, I'm a plugin for PlatformUI App!");
             console.log("And I'm plugged in ", app);
+
+            console.log("Let's add a route");
+            app.route({
+                name: "eZConfList",
+                path: "/ezconf/list",
+                view: "dashboardView", // let's display the dashboard since we don't have a custom view... yet :)
+                // we want the navigationHub (top menu) but not the discoveryBar
+                // (left bar), we can try different options
+                sideViews: {'navigationHub': true, 'discoveryBar': false},
+                callbacks: ['open', 'checkUser', 'handleSideViews', 'handleMainView'],
+            });
         },
     }, {
         NS: 'ezconfTypeApp' // don't forget that
