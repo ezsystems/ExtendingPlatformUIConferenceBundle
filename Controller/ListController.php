01c43cee899e295109060ab89a7ea705e8171cd0
Damien Pobel <dp@ez.no>
2015-10-28 17:15:31 +0100

    Added a Symfony controller to list contents

---
+++ Controller/ListController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EzSystems\ExtendingPlatformUIConferenceBundle\Controller;
+
+use EzSystems\PlatformUIBundle\Controller\Controller as BaseController;
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use  eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+
+class ListController extends BaseController
+{
+    private $searchService;
+
+    public function __construct(SearchService $searchService)
+    {
+        $this->searchService = $searchService;
+    }
+
+    public function listAction()
+    {
+        $query = new LocationQuery();
+        $query->query = new Criterion\Subtree('/1/');
+        return $this->render('EzSystemsExtendingPlatformUIConferenceBundle:List:list.html.twig', [
+            'results' => $this->searchService->findLocations($query),
+        ]);
+    }
+
+}
