7790a1948c0a096903a1ce7221fe440de7bf0264
Damien Pobel <dp@ez.no>
2015-10-28 18:20:51 +0100

    Implemented the list view service to do an AJAX request

--- Resources/public/js/views/services/ezconf-listviewservice.js
+++ Resources/public/js/views/services/ezconf-listviewservice.js
@@ -1,9 +1,27 @@
 YUI.add('ezconf-listviewservice', function (Y) {
     Y.namespace('eZConf');
 
-    Y.eZConf.ListViewService = Y.Base.create('ezconfListViewService', Y.eZ.ViewService, [], {
+    Y.eZConf.ListViewService = Y.Base.create('ezconfListViewService', Y.eZ.ServerSideViewService, [], {
         initializer: function () {
             console.log("Hey, I'm the ListViewService");
+        },
+
+        // _load is automatically called when the view service is configured for
+        // a route. callback should be executed when everything is finished
+        _load: function (callback) {
+            var uri = this.get('app').get('apiRoot') + 'list';
+
+            Y.io(uri, {
+                method: 'GET',
+                on: {
+                    success: function (tId, response) {
+                        this._parseResponse(response);
+                        callback(this);
+                    },
+                    failure: this._handleLoadFailure,
+                },
+                context: this,
+            });
         },
     });
 });
