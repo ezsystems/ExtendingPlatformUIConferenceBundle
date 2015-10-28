52ac48e20912267092cf18a7430eb9ce8916291d
Damien Pobel <dp@ez.no>
2015-10-28 18:22:07 +0100

    Changed the list view to be a server side view

--- Resources/public/js/views/ezconf-listview.js
+++ Resources/public/js/views/ezconf-listview.js
@@ -7,27 +7,10 @@ YUI.add('ezconf-listview', function (Y) {
     // * to identify the view if one wants to write a plugin for it
     // * to find the corresponding template
     // * to generate a CSS class on its container to ease styling
-    Y.eZConf.ListView = Y.Base.create('ezconfListView', Y.eZ.TemplateBasedView, [], {
+    Y.eZConf.ListView = Y.Base.create('ezconfListView', Y.eZ.ServerSideView, [], {
         initializer: function () {
             console.log("Hey, I'm the list view");
-        },
-
-        render: function () {
-            // this.get('container') is an auto generated <div>
-            // here, it's not yet in the DOM of the page and it will be added
-            // after the execution of render().
-
-            // when extending Y.eZ.TemplateBasedView
-            // this.template is the result of the template
-            // compilation, it's a function. You can pass an object
-            // in parameters and each property will be available in the template
-            // as a variable named after the property.
-            this.get('container').setHTML(
-                this.template({
-                    "name": "listView"
-                })
-            );
-            return this;
+            this.containerTemplate = '<div class="ez-view-ezconflistview"/>';
         },
     });
 });
