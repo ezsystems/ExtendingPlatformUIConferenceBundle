d8995695ee858eac26cf319dc94925fea5396660
Damien Pobel <dp@ez.no>
2015-10-29 08:09:09 +0100

    Added the ability to navigate to location from the list

--- Resources/public/js/views/ezconf-listview.js
+++ Resources/public/js/views/ezconf-listview.js
@@ -8,9 +8,38 @@ YUI.add('ezconf-listview', function (Y) {
     // * to find the corresponding template
     // * to generate a CSS class on its container to ease styling
     Y.eZConf.ListView = Y.Base.create('ezconfListView', Y.eZ.ServerSideView, [], {
+        // this is YUI View mechanic to subscribe to DOM events (click, submit,
+        // ...) and synthetic event (some custom event provided by YUI) like
+        // 'tap' here.
+        events: {
+            '.ezconf-list-location': {
+                // tap is 'fast click' (touch friendly)
+                'tap': '_navigateToLocation'
+            }
+        },
+
         initializer: function () {
             console.log("Hey, I'm the list view");
             this.containerTemplate = '<div class="ez-view-ezconflistview"/>';
+        },
+
+        _navigateToLocation: function (e) {
+            var link = e.target;
+
+            e.preventDefault(); // don't want the normal link behavior
+
+            // tell the view service we want to navigate somewhere
+            // it's a custom event that will be bubble up to the view service
+            // (and the app)
+            // the second parameter is the data to add in the event facade, this
+            // can be used by any event handler function bound to this event.
+            this.fire('navigateTo', {
+                routeName: link.getData('route-name'),
+                routeParams: {
+                    id: link.getData('route-id'),
+                    languageCode: link.getData('route-languagecode'),
+                }
+            });
         },
     });
 });
--- Resources/public/js/views/services/ezconf-listviewservice.js
+++ Resources/public/js/views/services/ezconf-listviewservice.js
@@ -4,6 +4,20 @@ YUI.add('ezconf-listviewservice', function (Y) {
     Y.eZConf.ListViewService = Y.Base.create('ezconfListViewService', Y.eZ.ServerSideViewService, [], {
         initializer: function () {
             console.log("Hey, I'm the ListViewService");
+
+            // we catch the `navigateTo` event no matter from where it comes
+            // when bubbling, the event is prefixed with the name of the
+            // component which fired the event first.
+            // so in this case we could also write
+            // this.on('ezconflistview:navigateTo', function (e) {});
+            // `e` is the event facade. It contains various informations about
+            // the event and if any the custom data passed to fire().
+            this.on('*:navigateTo', function (e) {
+                this.get('app').navigateTo(
+                    e.routeName,
+                    e.routeParams
+                );
+            });
         },
 
         // _load is automatically called when the view service is configured for
