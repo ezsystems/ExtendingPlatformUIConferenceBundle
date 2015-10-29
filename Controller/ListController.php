7e6f510ca3cf6e9f17ba2e6932d2e9148f5f9860
Damien Pobel <dp@ez.no>
2015-10-29 10:21:09 +0100

    Added the pagination links in the server output

--- Controller/ListController.php
+++ Controller/ListController.php
@@ -18,11 +18,25 @@ class ListController extends BaseController
 
     public function listAction($offset)
     {
+        $limit = 10;
         $query = new LocationQuery();
         $query->query = new Criterion\Subtree('/1/');
         $query->offset = (int)$offset;
+        $query->limit = $limit;
+        $results = $this->searchService->findLocations($query);
+
+        $previous = false;
+        $next = false;
+        if ( $offset > 0 ) {
+            $previous = max(0, $offset - $limit);
+        }
+        if ( ($offset + $limit) < $results->totalCount ) {
+            $next = $offset + $limit;
+        }
         return $this->render('EzSystemsExtendingPlatformUIConferenceBundle:List:list.html.twig', [
-            'results' => $this->searchService->findLocations($query),
+            'results' => $results,
+            'previous' => $previous,
+            'next' => $next,
         ]);
     }
 
